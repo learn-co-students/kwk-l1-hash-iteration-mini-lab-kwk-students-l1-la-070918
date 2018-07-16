b547357929173e1ed8edb94174232b32d45e7798
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-16 18:30:57 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,24 @@
+puts "THE SUMMER OLYMPICS"
+place = ["Sydney", "Athens", "Beijing", "London", "Atlanta"]
+year = [2000, 2004, 2008, 2012, 1996]
+summer_olympics = 
+{
+   :Sydney => 2000, 
+   :Athens => 2004, 
+   :Beijing => 2008, 
+   :London => 2012
+ }
+ puts summer_olympics
+ puts " "
 
-def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
-end
+puts "UPDATED SUMMER OLYMPICS"
+summer_olympics[:Atlanta] = 1996
+  puts summer_olympics
+  puts ""
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
+summer_olympics.each do |place, year|
+  puts "The #{year} summer olympics took place in #{place}."
+end 
 
-def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
+summer_olympics.each_key{|key|puts key.upcase}
 
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-end
